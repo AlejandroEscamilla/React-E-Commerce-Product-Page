5f7bc3f519b59a90fd6524ec39d57b9a81bba89b
AlejandroEscamilla <alejandroe.ordaz@gmail.com>
2020-05-18 09:55:04 -0400

    api test

---
+++ src/api.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class api extends React.Component {
+    userNameInput = React.createRef();
+    handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const resp = await axios.get(`https://api.github.com/users/${this.userNameInput.current.value}`);
+        this.props.onSubmit(resp.data);
+        this.userNameInput.current.value = '';
+        // console.log('submitted');
+        // console.log(this.userNameInput.current.value);
+        // console.log(resp.data);
+      } catch (error){
+        console.error(error);
+        console.log('Houston we have a problem!');
+      }
+    }
+    render(){
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" 
+          placeholder="Github username" 
+          ref={this.userNameInput} 
+          required/>
+          <button>Add card</button>
+        </form>
+      );
+    }
+  }
+
+  export default api;
